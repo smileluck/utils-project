18c475128ddf63263f6bde738fb942cc810651c2
smileluck <12386904+smileluck@users.noreply.github.com>
2022-11-11 11:52:01 +0800

    update

--- src/main/java/top/zsmile/runner/TestFileWalkFileTree.java
+++ src/main/java/top/zsmile/runner/TestFileWalkFileTree.java
@@ -1,32 +1,35 @@
 package top.zsmile.runner;
 
 
-
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TestFileWalkFileTree {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         AtomicInteger dirCount = new AtomicInteger();
         AtomicInteger fileCount = new AtomicInteger();
-        Files.walkFileTree(Paths.get("D:\\project\\Senlink\\aojia\\澳嘉文化官网\\doc\\联系我们"),new SimpleFileVisitor<Path>(){
+        Files.walkFileTree(Paths.get("D:\\my\\project\\SmileX-boot\\smilex-study\\doc"), new SimpleFileVisitor<Path>() {
             @Override
             public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                System.out.println("文件夹："+dir);
+//                System.out.println("文件夹："+dir);
                 dirCount.incrementAndGet();
                 return super.preVisitDirectory(dir, attrs);
             }
 
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                System.out.println("文件"+file);
+
+//                System.out.println("文件" + file);
+                if (file.getFileName().toString().endsWith(".md") || file.getFileName().toString().endsWith(".MD")) {
+                    System.out.println(file.getFileName().toString());
+                }
                 fileCount.incrementAndGet();
                 return super.visitFile(file, attrs);
             }
         });
-        System.out.println("dir count:"+dirCount);
-        System.out.println("file count:"+fileCount);
+        System.out.println("dir count:" + dirCount);
+        System.out.println("file count:" + fileCount);
     }
 }
